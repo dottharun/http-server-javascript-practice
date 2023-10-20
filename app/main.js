4bf1d968dae735c0eb8c28c195340940837fc22a
ubutharun <tharun242424@gmail.com>
2023-10-20 17:18:33 +0530

    4th try3

--- app/main.js
+++ app/main.js
@@ -12,17 +12,20 @@ const server = net.createServer((socket) => {
       .filter((i) => i.length > 0);
     const [method, path, version] = start_line.split(` `);
 
+    //headers
+    const user_agent = headers[1].split(' ')[1];
+
     let response = '';
 
     if (path === '/') {
       response = `HTTP/1.1 200 OK\r\n\r\n`;
-    } else {
-      response = `HTTP/1.1 404 Not Found\r\n\r\n`;
-    }
-
-    if (path.includes('/echo')) {
+    } else if (path.includes('/echo')) {
       const content = path.substring(6);
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+    } else if (path.includes('/user-agent')) {
+      response = `HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: ${user_agent.length}\r\n\r\n${user_agent}`;
+    } else {
+      response = `HTTP/1.1 404 Not Found\r\n\r\n`;
     }
 
     socket.write(response);
